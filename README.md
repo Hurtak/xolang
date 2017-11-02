b1a8c42e5f7cbeea158f68869d0b95206b3c829b
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2017-11-03 00:29:57 +0100

    First working version of AST

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 ## TODO
 
 - https://github.com/thejameskyle/the-super-tiny-compiler/blob/master/the-super-tiny-compiler.js
+- ast
+    - fully type compile function
+    - cleanup walk functions - so the do not use global state that much
 - compiler stuff
-    - create AST
     - transformation
     - js code generation
+
+- Prefixing interfaces with "I", is this common practice or should we disable this in TSLint?
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,29 @@ enum TokenType {
   Whitespace = "WHITESPACE",
 
   Name = "NAME",
+
   Number = "NUMBER",
+  String = "STRING",
 
   Parentheses = "PARENTHESES",
   Braces = "BRACES",
 }
+
+enum ASTType {
+  NumberLiteral = "NumberLiteral",
+  StringLiteral = "StringLiteral",
+  CallExpression = "CallExpression",
+  FunctionDeclaration = "FunctionDeclaration",
+  Program = "Program",
+}
+
+// type TokenTypeWhitespace = " " | "\t";
+// type TokenTypeName = string;
+// type TokenTypeNumber = number;
+// type TokenTypeString = string;
+// type TokenTypeStringIndicator = '"';
+// type TokenTypeParentheses = "(" | ")";
+// type TokenTypeBraces = "{" | "}";
 
 interface IToken {
   type: TokenType;
@@ -60,6 +78,27 @@ function tokenize(source: string): IToken[] {
       continue;
     }
 
+    const isCharStringToken = (s: string): boolean => /["]/.test(s);
+    if (isCharStringToken(char)) {
+      let value = "";
+
+      nextChar(); // Skip opening `"` char.
+
+      while (!isCharStringToken(char)) {
+        value += char;
+        nextChar();
+      }
+
+      nextChar(); // Skip closing `"` char.
+
+      tokens.push({
+        type: TokenType.String,
+        value: value,
+      });
+
+      continue;
+    }
+
     const isCharNumberToken = (s: string): boolean => /[0-9]/.test(s);
     if (isCharNumberToken(char)) {
       let value = "";
@@ -100,6 +139,80 @@ function tokenize(source: string): IToken[] {
   return tokens;
 }
 
+function parser(tokens: IToken[]) {
+  function getToken(offset = 0) {
+    return tokens[index + offset];
+  }
+
+  let index = 0;
+  let token = getToken();
+
+  function walk() {
+    function walkToken() {
+      index += 1;
+      token = getToken();
+    }
+
+    if (token.type === TokenType.Number) {
+      const data = {
+        type: ASTType.NumberLiteral,
+        value: token.value,
+      };
+      walkToken();
+
+      return data;
+    }
+
+    if (token.type === TokenType.String) {
+      const data = {
+        type: ASTType.StringLiteral,
+        value: token.value,
+      };
+      walkToken();
+
+      return data;
+    }
+
+    if (token.type === TokenType.Name) {
+      const node = {
+        type: ASTType.CallExpression,
+        name: token.value,
+        params: [],
+      };
+
+      walkToken();
+
+      if (token.type === TokenType.Parentheses && token.value === "(") {
+        walkToken(); // Skip `(` after function name
+
+        while (
+          token.type !== TokenType.Parentheses ||
+          (token.type === TokenType.Parentheses && token.value !== ")")
+        ) {
+          node.params.push(walk());
+        }
+
+        walkToken(); // Skip `)`
+
+        return node;
+      }
+    }
+
+    throw new Error(`Parser did not reckognize tocken type "${token.type}"`);
+  }
+
+  const ast = {
+    type: ASTType.Program,
+    body: [],
+  };
+
+  while (index < tokens.length) {
+    ast.body.push(walk());
+  }
+
+  return ast;
+}
+
 function main(): void {
   const src = fs.readFileSync(
     path.join(__dirname, "../program/main.xo"),
@@ -107,9 +220,10 @@ function main(): void {
   );
 
   const tokens = tokenize(src);
+  const ast = parser(tokens);
 
   process.stdout.write(
-    util.inspect(tokens, {
+    util.inspect(ast, {
       showHidden: false,
       depth: null, // default 2, null to unlimited
       colors: true,
